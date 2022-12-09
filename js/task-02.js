4f49b399232cfcbe483decc57e812b04623ac1b9
Domisiaxa <domisiaxa@gmail.com>
2022-12-09 19:10:22 +0100

    TASK 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,17 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsList = document.getElementById("ingredients");
+
+for (let ingredient of ingredients) {
+  const ingredientEl = document.createElement("li");
+  ingredientEl.innerText = ingredient;
+  ingredientEl.classList.add("item");
+  ingredientsList.appendChild(ingredientEl);
+}
