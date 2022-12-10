306898b0392bfb8fa93d353552d796b4339aa3ce
Domisiaxa <domisiaxa@gmail.com>
2022-12-10 16:45:55 +0100

    TASK 7

--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,8 @@
-let move = document.getElementById("font-size-control");
-let text = document.getElementById("text");
-move.addEventListener("input", () => {
-  let size = move.value;
-  text.style.fontSize = size + "px";
+const slider = document.querySelector(`#font-size-control`);
+const span = document.querySelector("#text");
+
+span.style.fontSize = `${slider.value}px`;
+
+slider.addEventListener("input", function (event) {
+  span.style.fontSize = event.currentTarget.value + "px";
 });
